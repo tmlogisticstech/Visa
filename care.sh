08dc94ae7c03fee2501ec6b82413f40d3d679d33
salaudeen hammed <martianprints20@gmail.com>
2023-02-12 23:21:28 +0000

    sharing love

---
+++ care.sh
@@ -0,0 +1,15 @@
+#This script is about wellbeing
+echo "What is your name?"
+read name 
+echo "Where are you from?"
+read
+echo "No wonder your face looks famiiar"
+read
+echo "How long have you lived in ajao estate?"
+read
+echo "I was born and brought up in ajao estate too"
+read
+echo "Yes"
+read
+echo "it is a pleasure meeting you $name"
+read 
